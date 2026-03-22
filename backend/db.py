0a69dcc09702a863eaeade7aabcb5000d6617d50
Shreya Ajith <shreyaajith480@gmail.com>
2026-03-22 14:33:39 +0530

    Updated db.py for Render env variables

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,11 @@
 import mysql.connector
+import os
 
 def get_connection():
     return mysql.connector.connect(
-        host="gondola.proxy.rlwy.net",
-        port=54476,
-        user="root",
-        password="sWVAKcHhEtJpdamyTFYruoEleyhxBtcP",
-        database="railway"
+        host=os.environ.get("DB_HOST"),
+        port=int(os.environ.get("DB_PORT", 3306)),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        database=os.environ.get("DB_NAME")
     )
